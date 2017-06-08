928f01a8caf0d9415ce995bf5cc5ce39950c18c3
Kalin Kostov core.editor = 'C:/Program Files (x86)/Notepad++/notepad++.exe' -multiInst -notabbar -nosession -noPlugin <k.kostov.eu@gmail.com>
2017-06-08 13:47:58 +0300

    entity repository created

---
+++ Application/KartRacingManager/KartRacingManager.Data/Repositories/EntityFrameworkRepository.cs
@@ -0,0 +1,55 @@
+﻿using KartRacingManager.Interfaces.Data;
+using System.Data.Entity;
+using System.Linq;
+
+namespace KartRacingManager.Data.Repositories
+{
+    public class EntityFrameworkRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        private readonly IDbContext data;
+        private readonly IDbSet<TEntity> set;
+
+        public EntityFrameworkRepository(IDbContext data)
+        {
+            this.data = data;
+            this.set = data.Set<TEntity>();
+        }
+
+        public void Add(TEntity entity)
+        {
+            this.ChangeState(entity, EntityState.Added);
+        }
+
+        public IQueryable<TEntity> All()
+        {
+            return this.set;
+        }
+
+        public TEntity FindById(object id)
+        {
+            return this.set.Find(id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            this.ChangeState(entity, EntityState.Deleted);
+        }
+
+        public void Update(TEntity entity)
+        {
+            this.ChangeState(entity, EntityState.Modified);
+        }
+
+        public void SaveChanges()
+        {
+            this.data.SaveChanges();
+        }
+
+        private void ChangeState(TEntity entity, EntityState state)
+        {
+            var dbEntry = this.data.Entry(entity);
+            dbEntry.State = state;
+        }
+    }
+}
---
+++ Application/KartRacingManager/KartRacingManager.Interfaces/Data/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace KartRacingManager.Interfaces.Data
+{
+    public interface IRepository<TEntity>
+    {
+        IQueryable<TEntity> All();
+
+        TEntity FindById(object id);
+
+        void Add(TEntity entity);
+
+        void Remove(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void SaveChanges();
+    }
+}
